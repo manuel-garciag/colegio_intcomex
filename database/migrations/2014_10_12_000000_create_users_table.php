a0f55ee0b5cce32aa96132634d24f259970abc51
Manuel <garciahildo@gmail.com>
2022-03-13 14:09:06 -0500

    Se crea campo rol y se crean 3 usuarios para demo del aplicativo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,9 +19,40 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('rols_id')->unsigned()->default(3); //Foranea con tabla de roles - Por defecto queda en 3 para que se registren solo estudianes
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('rols_id')->references('id')->on('rols')->nullable(false); 
         });
+
+        //Insertamos los datos necesarios para el manejo de los usuarios
+        // Password por defecto 1234abcd
+        $roles = array(
+            [
+                'name' => 'Admin', 
+                'email' => 'admin@colint.com', 
+                'password' => '$2y$10$/FqRBsNyDXFfCGZIxAndie4E7DBSFZdF6sRanFvanBV7nNnJqjKa.',
+                'rols_id' => 1
+            ],
+            [
+                'name' => 'Docente', 
+                'email' => 'docente@colint.com', 
+                'password' => '$2y$10$/FqRBsNyDXFfCGZIxAndie4E7DBSFZdF6sRanFvanBV7nNnJqjKa.',
+                'rols_id' => 2
+            ],
+            [
+                'name' => 'Estudiante', 
+                'email' => 'estudiante@colint.com', 
+                'password' => '$2y$10$/FqRBsNyDXFfCGZIxAndie4E7DBSFZdF6sRanFvanBV7nNnJqjKa.',
+                'rols_id' => 3
+            ]
+        );
+
+        for ($i=0; $i < count($roles); $i++) { 
+        DB::table("users")
+        ->insert($roles[$i]);
+        }
     }
 
     /**
