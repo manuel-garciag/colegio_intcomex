9c5f49688858f19c1a98fa236922fc07acae28ad
Manuel <garciahildo@gmail.com>
2022-03-12 22:45:28 -0500

    Creacion de migracion para las asignaturas

---
+++ database/migrations/2022_03_13_033658_create_subjects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->char('name', 100)->nullable(false);
+            $table->tinyInteger('status')->default(1)->comment('0 Inactivo | 1 Activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
